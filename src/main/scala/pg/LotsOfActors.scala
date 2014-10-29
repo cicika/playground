c638a59dead32249a39bd55702eaa507cc3b0513
Tijana Dojcinovic <tdojcinovic@groundlink.com>
2014-10-29 18:19:02 +0100

    ...

--- src/main/scala/pg/LotsOfActors.scala
+++ src/main/scala/pg/LotsOfActors.scala
@@ -1,6 +1,7 @@
 package pg
 
 import akka.actor._
+import akka.actor.Actor._
 
 import java.util.Date
 
@@ -17,6 +18,9 @@ object Main extends App {
 	val t1 = timestamp
 
 	for(i <- 0 to n) {
+		//This creates a race condition if the number of actors being started is small.
+		//We've seen this happen when running tests with 1,000,000 actors
+		//never with 10m
 		Future{
 			system.actorOf(Props[Worker], "worker-%d" format i)
 		}
@@ -29,19 +33,19 @@ object Main extends App {
 
 	class Worker extends Actor {
 		def receive = {
-			case x: Int =>
-				if (x < n - 1) {
-					context.actorSelection("/user/worker-%d" format (x + 1)) ! (x + 1)
-					context stop self
-				} else {
-					val t4 = timestamp
-					println("Simulation completed in %d milis. Result %d" format ((t4 - t3), x))
-					context.system.shutdown
-				}
-				
-			case _ =>
+		case x: Int =>
+			if (x < n - 1) {
+				context.actorSelection("/user/worker-%d" format (x + 1)) ! (x + 1)
+				context stop self
+			} else {
+				val t4 = timestamp
+				println("Simulation completed in %d milis. Result %d" format ((t4 - t3), x))
+				context.system.shutdown
+			}			
+		case _ =>
 		}
 	}
 
 	def timestamp = new Date().getTime()
+
 }
